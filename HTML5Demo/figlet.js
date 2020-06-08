bde2d64dee11b51ce6d45134d8cb97c6a68445d6
Jermaine <jermerf@gmail.com>
2020-06-08 10:26:48 -0300

    Initial commit with sample HTML5 notes

---
+++ HTML5Demo/figlet.js
@@ -0,0 +1,10 @@
+const figlet = require('figlet')
+
+
+figlet("Hello World!",{font: "Gothic"}, (err, data) => {
+  if(err) {
+    console.log("Something went wrong")
+  }else{
+    console.log(data)
+  }
+})
